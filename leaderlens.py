ee3dfad3580610235f0c89185bc8b88dca8d2d19
Chris Morgado <cmmorgado53@gmail.com>
2025-11-27 12:57:08 -0500

    parsing data

--- leaderlens.py
+++ leaderlens.py
@@ -1,7 +1,24 @@
-import pandas as pd
+import pandas as pd 
+import typer
 
-def main():
-    pass
+app = typer.Typer()
 
+@app.command()
+def peer(semester: str = "2251"):
+    try:
+        print(f"Finding peer observation data...")
+        df = pd.read_csv(f"data/peer{semester}.csv")
+        print(df.iloc[78])
+    except FileNotFoundError:
+        print("Data not found.")
+
+@app.command()
+def full(semester: str = "2251"):
+    print(f"Finding full observation data...")
+
+@app.command()
+def walkthrough(semester: str = "2251"):
+    print(f"Finding walkthrough observation data...")
+    
 if __name__ == "__main__":
-    main()
+    app()
